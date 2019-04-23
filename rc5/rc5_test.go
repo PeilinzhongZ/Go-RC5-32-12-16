f4816137ef9b50cbcb4731e72ca6bc2401726e83
PeilinZhong <31450172+PeilinzhongZ@users.noreply.github.com>
2019-04-22 22:32:15 -0700

    update 5

--- rc5/rc5_test.go
+++ rc5/rc5_test.go
@@ -3,7 +3,11 @@ package rc5
 import (
 	"bytes"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"testing"
 )
 
@@ -32,24 +36,48 @@ func TestRC5_Random(t *testing.T) {
 	}
 }
 
-func TestRC5_TESTVECTORS(t *testing.T) {
+func TestRC5_TESTVECTORS_R12(t *testing.T) {
+	vectors := readJSON("../TestVectors/RC5_TestVectors_r12.json")
+	runTest(vectors, t)
+}
+
+// func TestRC5_TESTVECTORS_R20(t *testing.T) {
+// 	vectors := readJSON("../TestVectors/RC5_TestVectors_r20.json")
+// 	runTest(vectors, t)
+// }
+
+func runTest(vectors []map[string]string, t *testing.T) {
 	var encrypted [8]byte
 	var decrypted [8]byte
+	for _, item := range vectors {
+		key, _ := hex.DecodeString(item["Key"])
+		pt, _ := hex.DecodeString(item["Input"])
+		ct, _ := hex.DecodeString(item["Output"])
 
-	key, _ := hex.DecodeString("5269F149D41BA0152497574D7F153125")
-	pt, _ := hex.DecodeString("65C178B284D197CC")
-	ct, _ := hex.DecodeString("EB44E415DA319824")
+		cipher, ok := RC5_SETUP(key)
+		if ok {
+			cipher.RC5_ENCRYPT(pt[:], encrypted[:])
+			if !bytes.Equal(ct[:], encrypted[:]) {
+				t.Errorf("encryption/decryption failed: % 02x != % 02x\n", encrypted, ct)
+			}
 
-	cipher, ok := RC5_SETUP(key)
-	if ok {
-		cipher.RC5_ENCRYPT(pt[:], encrypted[:])
-		if !bytes.Equal(ct[:], encrypted[:]) {
-			t.Errorf("encryption/decryption failed: % 02x != % 02x\n", encrypted, ct)
-		}
-
-		cipher.RC5_DECRYPT(encrypted[:], decrypted[:])
-		if !bytes.Equal(decrypted[:], pt[:]) {
-			t.Errorf("encryption/decryption failed: % 02x != % 02x\n", decrypted, pt)
+			cipher.RC5_DECRYPT(encrypted[:], decrypted[:])
+			if !bytes.Equal(decrypted[:], pt[:]) {
+				t.Errorf("encryption/decryption failed: % 02x != % 02x\n", decrypted, pt)
+			}
 		}
 	}
+}
+
+func readJSON(path string) []map[string]string {
+	jsonFile, err := os.Open(path)
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var result []map[string]string
+	json.Unmarshal([]byte(byteValue), &result)
+	return result
 }
